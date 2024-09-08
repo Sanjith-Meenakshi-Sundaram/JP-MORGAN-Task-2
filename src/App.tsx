d52264a875b32217b592588a428c73dfb5e2ac3c
Meenakshi Sundaram D <sanjithdhamu@gmail.com>
2024-09-08 09:52:07 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,7 @@ class App extends Component<{}, IState> {
     if(x>1000){
       clearInterval(interval);
     }
+  },100)
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
